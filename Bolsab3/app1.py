1ff573260e300265ecfe5f5067ec46edc1034edd
Juliocwb <julio.cwb@gmail.com>
2022-11-28 09:36:14 -0300

    update

--- Bolsab3/app1.py
+++ Bolsab3/app1.py
@@ -12,4 +12,8 @@ colors= (
     'text' : '#7FDBFF'
 )
 
-df = pd.read_csv('')
+df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/9166432/raw/0d5410f1f8da88670adc86080e7154009ebf4906/indicator%2520gapminder%2520gdp_per_capita_ppp%2520-%2520Data.csv')
+
+fig_scater = px.scater(df, x="gpd per capita", y="Life expectation".
+             size= popu       )
+             
